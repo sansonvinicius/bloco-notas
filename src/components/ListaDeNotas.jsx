e74fe83945f4a2d77b9ae01eeb9c38869681c253
Vinicius Henrique Sanson <sansonvinicius@MacBook-Air-de-Vinicius.local>
2021-09-16 11:56:05 -0300

    alterado ListaDeNotas para mapear as categorias

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -4,18 +4,21 @@ import CardNota from "./CardNota"
 class ListaDeNotas extends Component {
 
     //Precisa ter o método render para renderizar a página
+    //Para inserir js precisa colocar entre {}
     render() {
         return (
             <ul>
-                <li>
-                    <CardNota/>
-                </li>
-                <li>
-                     <CardNota/>
-                </li>
+                {/* Mapeia um array para inserir as categorias das notas */}
+                {Array.of("Trabalho","Trabalho","Estudos").map((categoria) => {
+                    return (
+                        <li>
+                            <div>{categoria}</div>
+                            <CardNota/>
+                        </li>
+                    );
+                } )}
             </ul>
         );
     }
-
 }
 export default ListaDeNotas;
