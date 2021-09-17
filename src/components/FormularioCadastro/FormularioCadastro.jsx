1d0a837a5af3f1a051359c71f50232824ce90278
Vinicius Henrique Sanson <sansonvinicius@MacBook-Air-de-Vinicius.local>
2021-09-17 11:40:11 -0300

    desenvolvendo método handleMudancaTitulo

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react"
 import "./estilo.css"
 
  class FormularioCadastro extends Component{
+
+    constructor(){
+        super();
+        this.titulo="";
+    }
+    handleMudancaTitulo(evento){
+        this.titulo = evento.target.value;
+        console.log(this.titulo);
+    }
+
     render(){
         return(
             <form className="form-cadastro">
@@ -9,6 +19,7 @@ import "./estilo.css"
                     type="text" 
                     placeholder="Titulo" 
                     className="form-cadastro_input" 
+                    onChange={this.handleMudancaTitulo.bind(this)}
                 />
                 <textarea
                     rows={15}
