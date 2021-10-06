b500cf4d19fac51b3f1db871658dfad9511bf23a
Vinicius Henrique Sanson <sansonvinicius@192.168.2.8>
2021-10-06 14:21:32 -0300

    ajustando icone no Cardnota.jsx

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import "./estilo.css"
-import deleteSVG from "../../assets/img/delete.svg"
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg"
 
 // o className identifica as classes atribuidas no css
 class CardNota  extends Component {
@@ -15,7 +15,7 @@ class CardNota  extends Component {
             <section className="card-nota">
               <header className="card-nota_cabecalho">
                 <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-                <img src={deleteSVG} onClick={this.apagar.bind(this)}/>
+                <DeleteSVG onClick={this.apagar.bind(this)}/>
                 <h4>{this.props.categoria}</h4>
               </header>
               <p className="card-nota_texto">{this.props.texto}</p>
